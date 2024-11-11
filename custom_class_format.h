3219c9caf8cd308ff16becf98e755312a001c21e
Chun Ho Chan <ccha232@uwo.ca>
2024-11-11 01:33:14 -0500

    Created a script to generate new header/C++ files.

---
+++ custom_class_format.h
@@ -0,0 +1,38 @@
+/**
+ * @file ___class_name_here___.h
+ * @author
+ * @brief
+ * @version
+ * @date
+ * 
+ */
+
+#ifndef ___CLASS_NAME_HERE____H
+#define ___CLASS_NAME_HERE____H
+
+#include <godot_cpp/core/class_db.hpp> // This hold the list of all registered classes
+#include <godot_cpp/classes/node.hpp> // Bindings to the parent class
+#include "boilerplate_macros.h" // Macros for GDExtension's boilerplate code
+
+namespace godot {
+
+	class ___ClassNameHere___ : public Node {
+		
+		GDCLASS(___ClassNameHere___, Node) // A Godot macro for class inheritance
+		_GDEXPORT // A custom macro for exporting instance variables
+
+	private:
+
+	protected:
+		static void _bind_methods(); // Must be declared
+
+	public:
+		___ClassNameHere___();
+		~___ClassNameHere___();
+
+		void _process(double delta) override;
+	};
+
+} // namespace godot
+
+#endif
---
+++ generate_custom_class_h_cpp.py
@@ -0,0 +1,106 @@
+"""
+Description:
+    This Python script generates 'custom_class.h' and 'custom_class.cpp' in './src/'.
+Author:
+    Chun Ho Chan (Edward)
+Version:
+    v1.0 (2024-11-10)
+License:
+    GNU Affero General Public License v3.0
+"""
+
+import os
+
+def generate_file(class_name, file_extension):
+    """
+    Generates a file from template with the specified class name and file extension.
+    """
+    # Special token to indicate where to insert the class name
+    TOKEN = "___"
+
+    # List of valid commands
+    COMMANDS = [
+            "class_name_here", # Filename
+            "CLASS_NAME_HERE", # Include Guard
+            "ClassNameHere", # Class Name
+            "Class Name Here" # Doxygen Comment; User Input
+        ]
+    
+    # Generate all the name formats
+    names = []
+    names.insert(0, class_name.replace(" ", "_").lower())
+    names.insert(1, class_name.replace(" ", "_").upper())
+    names.insert(2, class_name.replace(" ", ""))
+    names.insert(3, class_name)
+
+    # Find the template file
+    file_extension = file_extension.lower()
+    if file_extension in [".h", ".cpp"]:
+        format_file = "custom_class_format" + file_extension
+        output_file = ".\\src\\" + names[0] + file_extension
+    else:
+        return # Invalid file extension
+
+    # Read/ Copy from the format file. Write to the header file
+    with open(format_file, mode="r", encoding="utf-8") as formatter, open(output_file, mode="w", encoding="utf-8") as output:
+        for line in formatter:
+            # Reset head to start of the line
+            head = 0
+
+            while True:
+                # Find the head token
+                head = line.find(TOKEN, head)
+                if head == -1:
+                    # Head token is not found
+                    output.write(line)
+                    break
+
+                # Find the tail token. 
+                tail = line.find(TOKEN, head + len(TOKEN))
+                if tail == -1:
+                    # Tail token is not found
+                    output.write(line)
+                    break
+
+                # Find and validate the text surrounded by the tokens
+                text = line[head + len(TOKEN) : tail]
+                if not text in COMMANDS:
+                    # Command is invalid
+                    output.write(line)
+                    break
+
+                # Find the text outside of the tokens
+                prefix = line[0 : head]
+                suffix = line[tail + len(TOKEN) : ]
+
+                # Execute the left-most command in this line
+                line = prefix + names[COMMANDS.index(text)] + suffix
+
+                # Update head to avoid scanning the replacement string
+                head = head + len(names[COMMANDS.index(text)])
+
+                # Check if no more token is following
+                if line.find(TOKEN, head) == -1:
+                    output.write(line)
+                    break
+            # repeat-until loop ends
+        # for loop ends
+
+def main():
+    """
+    Main method.
+    """    
+    # Get input
+    input_name = input("Enter the class name (format: \"GD Custom Class 1\"): ")
+    input_name.replace("\"", "").strip()
+
+    # Generate both files
+    generate_file(input_name, ".h")
+    generate_file(input_name, ".cpp")
+    
+    # Program successfully finished
+    print("Done!")
+
+# Entry Point
+if __name__ == "__main__":
+    main()
