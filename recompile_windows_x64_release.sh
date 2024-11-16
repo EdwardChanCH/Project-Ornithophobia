0c6b6227b5efe48b5b8e8071fc7b0082820cd604
Chun Ho Chan <ccha232@uwo.ca>
2024-11-15 20:30:02 -0500

    Updated Bash scripts printouts.

--- recompile_windows_x64_release.sh
+++ recompile_windows_x64_release.sh
@@ -11,21 +11,21 @@ License:
     GNU Affero General Public License v3.0
 COMMENT
 
-printf "> Executing Bash script...\n"
+printf "> Executing Bash script ...\n"
 printf "> Setting: 64-bit Windows, Release mode\n"
 
 input='r'
 while [[ $input == [rR] ]]
 do
-    printf "\n> Generating 'register_types.cpp' with Python...\n"
+    printf "\n> Generating 'register_types.cpp' with Python ...\n"
     python generate_register_types_cpp.py
     printf "Done. (exit code: %d)\n" $?
 
-    printf "\n> Compiling GDExtension modules with SCon...\n"
+    printf "\n> Compiling GDExtension modules with SCon ...\n"
     scons platform=windows target=template_release
     printf "Done. (exit code: %d)\n" $?
 
     printf "\n> Compilation finished. You may open Godot and run/ edit your project.\n"
     read -p "> Press 'q' to exit, 'r' to recompile: " -n 1 input
-    printf "\n"
+    printf "\n\n> ------------------------------\n"
 done
