90a7bd69819955ebddc5bc218377ae2361dd0158
Edward Chan <edwardchanchunho@gmail.com>
2024-12-16 16:00:32 -0500

    Changed GUT settings.

--- game/gut_test/test_example.gd
+++ game/gut_test/test_example.gd
@@ -4,7 +4,7 @@ func test_assert_true() -> void:
 	# Pretend this result is from a function call
 	var result: bool = true
 	
-	# Format: function_name(actual_outcome, expected_outcome, fail_message)
+	# Format: function_name(actual_outcome, expected_outcome, test_message)
 	assert_eq(result, true, "This should be true.") # Assert Equal
 	
 	pass # End of function
