d478a124e9aa33414b9caf21e027f7d97592e7cd
Jacob Couture <27429011+SpeedBerry@users.noreply.github.com>
2024-12-19 23:11:39 -0500

    Adjusted in-game background and sfx

--- game/script/clickSFX_player.gd
+++ game/script/clickSFX_player.gd
@@ -8,12 +8,11 @@ static var left = preload("res://asset/audio/leftSFX.mp3")
 static var right = preload("res://asset/audio/rightSFX.mp3")
 
 
-func _on_left_click() -> void:
-	play_left()
-
-
-func _on_right_click() -> void:
-	play_right()
+func _input(event: InputEvent) -> void:
+	if (event.is_action_pressed("small_blast")):
+		play_left()
+	elif (event.is_action_pressed("large_blast")):
+		play_right()
 
 
 func _process(_delta: float) -> void:
